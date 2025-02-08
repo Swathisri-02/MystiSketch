2e676c50b1ae59989e2502b5e3ede598f07ad2d4
Swathisri-02 <swathisricodes@gmail.com>
2025-02-08 23:30:32 +0530

    Added login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,12 +2,54 @@ import React,{ useState, useEffect, useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import { motion } from "motion/react"
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
   const [state, setState] = useState('Login')
+  const {setShowLogin, backendUrl, setToken, setUser} = useContext(AppContext);
 
-  const {setShowLogin} = useContext(AppContext);
+  const [name, setName] = useState('')
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+
+    try{
+      if(state === 'Login'){
+        const {data} = await axios.post(backendUrl + '/api/user/login',{email, password})
+
+        if(data.success){
+          setToken(data.token)
+          setUser(data.user)
+          localStorage.setItem('token', data.token)
+          setShowLogin(false)
+        }
+        else{
+          toast.error(data.message)
+        }
+      }
+      
+      else{
+        const {data} = await axios.post(backendUrl + '/api/user/register',{name,email, password})
+
+        if(data.success){
+          setToken(data.token)
+          setUser(data.user)
+          localStorage.setItem('token', data.token)
+          setShowLogin(false)
+        }
+        else{
+          toast.error(data.message)
+        }
+      }
+    } 
+    catch(error){
+      toast.error(error.message)
+    }
+  }
 
   useEffect(() => {
     document.body.style.overflow = 'hidden'
@@ -18,7 +60,7 @@ const Login = () => {
 
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-        <motion.form className='relative bg-white p-10 rounded-xl text-slate-500'
+        <motion.form onSubmit={onSubmitHandler} className='relative bg-white p-10 rounded-xl text-slate-500'
         initial={{ opacity: 0.2,y:50 }}
         transition={{ duration: 0.3 }}
         whileInView={{ opacity: 1,y:0 }}
@@ -32,17 +74,17 @@ const Login = () => {
               state !== 'Login' &&
               <div className='border px-6 py-2 flex items-center gap-2 mt-4 rounded-full'>
                 <img src={assets.profile_icon} className='h-5' alt="" />
-                <input type="text" className='outline-none text-sm' placeholder='Full Name' required />
+                <input onChange={(e) => setName(e.target.value)} value={name} type="text" className='outline-none text-sm' placeholder='Full Name' required />
             </div>}
 
             <div className='border px-6 py-2 flex items-center gap-2 mt-4 rounded-full'>
                 <img src={assets.email_icon} className='' alt="" />
-                <input type="email" className='outline-none text-sm' placeholder='Email id' required />
+                <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className='outline-none text-sm' placeholder='Email id' required />
             </div>
 
             <div className='border px-6 py-2 flex items-center gap-2 mt-4 rounded-full'>
                 <img src={assets.lock_icon} className='' alt="" />
-                <input type="password" className='outline-none text-sm' placeholder='Password' required />
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} className='outline-none text-sm' placeholder='Password' required />
             </div>
 
             <p className='text-sm my-4 text-blue-600 cursor-pointer'>Forgot Password ?</p>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,10 +5,15 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
     const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false)
+    const [token, setToken] = useState(localStorage.getItem('token'))
+
+    const [credits, setCredits] = useState(false)
     
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const value = {
-        user,setUser,showLogin,setShowLogin
+        user,setUser,showLogin,setShowLogin, backendUrl, token, setToken, credits, setCredits
     }
 
     return (
