0eabc9caf8f6fd4ffa5d84ef311ec153eb760a1b
Swathisri-02 <swathisricodes@gmail.com>
2025-02-09 17:21:25 +0530

    Updation of images

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -7,8 +7,7 @@ import star_icon from './star_icon.svg'
 import rating_star from './rating_star.svg'
 import sample_img_1 from './sample_img_1.png'
 import sample_img_2 from './sample_img_2.png'
-import profile_img_1 from './profile_img_1.png'
-import profile_img_2 from './profile_img_2.png'
+import sample_img_3 from './sample_img_3.png'
 import step_icon_1 from './step_icon_1.svg'
 import step_icon_2 from './step_icon_2.svg'
 import step_icon_3 from './step_icon_3.svg'
@@ -29,6 +28,7 @@ export const assets = {
     rating_star,
     sample_img_1,
     sample_img_2,
+    sample_img_3,
     email_icon,
     lock_icon,
     cross_icon,
@@ -55,29 +55,6 @@ export const stepsData = [
     },
   ];
 
-export const testimonialsData = [
-    {
-        image:profile_img_1,
-        name:'Donald Jackman',
-        role:'Graphic Designer',
-        stars:5,
-        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
-    },
-    {
-        image:profile_img_2,
-        name:'Richard Nelson',
-        role:'Content Creator',
-        stars:5,
-        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
-    },
-    {
-        image:profile_img_1,
-        name:'Donald Jackman',
-        role:' Graphic Designer',
-        stars:5,
-        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
-    },
-]
 
 export const plans = [
     {
