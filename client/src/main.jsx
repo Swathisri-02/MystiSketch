a111f8229f93452f4b402842fd2d3257cbc617b2
Swathisri-02 <swathisricodes@gmail.com>
2025-02-10 17:46:28 +0530

    Empty commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import AppContextProvider from './context/AppContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
-      <App />
+      <App /> 
     </AppContextProvider>
   </BrowserRouter>
 )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const home = () => {
       <Header />
       <Steps />
       <Description />
-      <GenerateBtn />
+      <GenerateBtn /> 
       
     </div>
   )
